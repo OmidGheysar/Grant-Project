75e1026557e149398a5ce8cb8ebd3560ffefeea7
OmidGheysar <omid.geysar@gmail.com>
2021-04-26 17:27:12 -0700

    Character to factor level and number, reverse!

--- learn Igraph.R
+++ learn Igraph.R
@@ -40,14 +40,37 @@ library(optrees)
 nodes <- 1:7
 arcs <- matrix(c(1,2,2, 1,3,15, 1,4,3, 2,3,1, 2,4,9, 3,4,1),
                ncol = 3, byrow = TRUE)
-arcs <- matrix(cbind(DF$from,DF$to,DF$weights),
-               ncol = 3, byrow = TRUE)
+
 # Minimum cost spanning tree with several algorithms
 tree <- getMinimumSpanningTree( nodes, arcMatrix, algorithm = "Prim")
-getMinimumSpanningTree(as.vector(nodes), myMatrix, algorithm = "Kruskal")
+tree1 <- getMinimumSpanningTree(as.numeric(factor(nodes)), arcMatrix, algorithm = "Kruskal")
 getMinimumSpanningTree(nodes, arcs, algorithm = "Boruvka")
 
 network <- as.data.frame(tree[[2]])
+
+data <- c("red","blue","green","red","yellow", "red", "blue")
+factor_data <- factor(data)
+num_data <- as.numeric(factor_data)
+levels(factor_data)[num_data]
+
+head(myData,100)
+factor_data <- factor(myData$V1)
+num_data <- as.numeric(factor_data)
+levels(factor_data)[num_data]
+
+identifiers <- c(DF$from,DF$to)
+factor_data <- factor(identifiers)
+length(as.numeric(factor(identifiers)))
+allVector <- as.numeric(factor(identifiers))
+size = length(as.numeric(factor(identifiers)))
+improvedEdges <- data.frame('from'=allVector[1:(size/2)],'to'=allVector[(size/2+1):size],'weights'=1:8)
+improvedEdgesReal <- data.frame('from'=levels(factor_data)[allVector[1:(size/2)]],
+                                'to'=levels(factor_data)[allVector[(size/2+1):size]],'weights'=1:8)
+arcs <- as.matrix(improvedEdges)
+
+tree1 <- getMinimumSpanningTree(as.numeric(factor(nodes)), arcs, algorithm = "Kruskal")
+levels(factor_data)[allVector]
+
 
 
 
